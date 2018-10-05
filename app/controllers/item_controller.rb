53978f72ecd85ea869597164fe630c27f93e75c2
Scott Morey <swmorey@gmail.com>
2018-10-05 15:47:46 -0400

    item controller tweaks and more fleshed out example of item details fancybox

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -12,12 +12,13 @@ class ItemController < ApplicationController
   private
 
   def allowed_params
-    params.permit(:links, :abstract, :publisher_location, :create_date,
-                :corpauthor, :electronic, :id, :abstract_array, :physical_description,
-                :title, :title_alt, :fiction, :source, :title_display,
+    params.permit(:links, :abstract, :publisher_location, :create_date, :isbn, 
+                :corpauthor, :electronic, :id, :physical_description, :series, :availability,
+                :title, :title_alt, :fiction, :source, :title_display, :abstract_array,
                 :contents, :holdable, :title_nonfiling, :contents_array, :edit_date,
                 :type_of_resource, :sort_year, :publisher, :title_short, :author, :hold_count,
                 :author_other, :record_year, isbn: [], series: [], attrs: {},
-                genres: [], holdings: {}, subjects: [], availability: {})
+                genres: [], holdings: {}, subjects: [], availability: {}, abstract_array: [],
+                contents_array: [], author_other: [], title_alt: [])
   end
 end
