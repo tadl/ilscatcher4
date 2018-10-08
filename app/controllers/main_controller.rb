d76bc57db5b2408033c6951dd006662092e17936
William Rockwood <wrockwood@tadl.org>
2018-10-08 16:56:33 -0400

    what I have
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,20 +2,28 @@ class MainController < ApplicationController
 
   def index
     @list_groups = Settings.lists
-    @carousel_sections = []
+    @carousel_sections = {}
     #@screensaver stuff for opac goes here, we might want to redo this so I'm not duplicating it -- yet.
 
     @list_groups.each do |lg|
       group = lg["group"]
+      section_lists = []
+
       lg["searches"].each do |s|
         list = {}
-        list[group] = {}
-        list[group]["title"] = s["name"]
-        list[group]["nice_title"] = s["display_name"]
-        list[group]["items"] = Rails.cache.read(s["name"])
-        @carousel_sections.push(list[group])
+        list["title"] = s["name"]
+        list["nice_title"] = s["display_name"]
+        list["items"] = Rails.cache.read(s["name"])
+       section_lists.push(list)
       end
 
+      @carousel_sections.store(group, section_lists)
+
+    end
+
+    respond_to do |format|
+      format.html
+      format.json {render :json => {:featured_items => @carousel_sections}}
     end
 
   end
