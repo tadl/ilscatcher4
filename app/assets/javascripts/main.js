ce460abfc06b3773cc298dea0ef88bdbb133c2cb
William Rockwood <wrockwood@tadl.org>
2018-10-09 11:40:10 -0400

    lazyload the carousel images, with high res for retina
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,16 +3,19 @@ $(document).ready(function() {
     loop: true,
     margin: 10,
     nav: false,
+    dots: true,
+    lazyLoad: true,
     responsiveClass: true,
     responsive: {
       0: {
         items: 1,
+        dots: false,
       },
       600: {
-        items: 4,
+        items: 3,
       },
       1000: {
-        items: 6,
+        items: 5,
         loop: false,
         margin: 20
       }
