72cf1caa768c7ead7a099d819746dde96c1d12bc
William Rockwood <wrockwood@tadl.org>
2018-10-15 16:17:15 -0400

    that's not going to work
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -12,13 +12,6 @@ class ItemController < ApplicationController
       search = Search.new(query: params[:id], type: 'record_id')
       search.get_results
       @item = search.results[0]
-      if @item.links
-        @item.links.each do |link|
-          if link.include? "via.tadl.org"
-            @item.eresource_link = link
-          end
-        end
-      end
     end
     respond_to do |format|
       format.html
