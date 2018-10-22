10624f9ca882074103cf9c0e4b1e32169f7fece7
William Rockwood <wrockwood@tadl.org>
2018-10-22 13:02:59 -0400

    fix bug introduced in previous commit
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item
                 :contents, :holdable, :title_nonfiling, :contents_array, :edit_date, :search_location,
                 :type_of_resource, :sort_year, :publisher, :title_short, :author, :hold_count,
                 :author_other, :record_year, :availability, :attrs, :eresource_link, :result_order,
-                :search_view,
+                :search_view
 
   def is_available(search)
     # returns bool if item is available at all or a single location depending on the search location param
