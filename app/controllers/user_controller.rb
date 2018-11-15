e46543521476a4a6e2405858dc967cabbdde398d
William Rockwood <wrockwood@tadl.org>
2018-11-15 10:37:16 -0500

    formatting/style so I can read it easier
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,28 +1,33 @@
 class UserController < ApplicationController
   respond_to :html, :json, :js
-  before_action :check_for_token, except: [:login, :missing_token] 
-  
+  before_action :check_for_token, except: [:login, :missing_token]
+
   def login
     if cookies[:login] || params[:token] || (params[:username] && (params[:password] || params[:md5password]))
       @user = User.new
+
       if cookies[:login] && (!params[:token] || !params[:username])
         params[:token] = cookies[:login]
       end
+
       @user.login(params)
+
       if @user.error == nil
-        cookies[:login] = {:value => @user.token, :expires => 1.hour.from_now.utc}
-        cookies[:user] = {:value => @user.to_json, :expires => 1.hour.from_now.utc}
+        basic_info_and_cookies(@user)
       else
         cookies.delete :login
         cookies.delete :user
       end
+
     else
       @user = {'error'=> 'missing parameters'}
     end
+
     respond_to do |format|
       format.json {render :json => @user}
       format.js
     end
+
   end
 
   def logout
@@ -32,23 +37,26 @@ class UserController < ApplicationController
       cookies.delete :user
       @message = {:success => 'logged out'}
     end
+
     respond_to do |format|
       format.json {render :json => @message}
       format.js
     end
+
   end
 
 #CHECKOUTS
-
   def checkouts
     if @user
       @checkouts = @user.TEMP_get_checkouts
       basic_info_and_cookies(@user)
     end
+
     respond_to do |format|
       format.html
       format.json {render :json =>{:user => @user, :checkouts => @checkouts}}
     end
+
   end
 
   def renew_checkouts
@@ -61,19 +69,23 @@ class UserController < ApplicationController
     else
       @checkouts = {:error => 'missing parameters'}
     end
+
     respond_to do |format|
-      format.json {render :json =>{:user => @user, :message => @message, :errors => @errors, 
+      format.js
+      format.json {render :json =>{:user => @user, :message => @message, :errors => @errors,
                           :checkouts => @checkouts}}
     end
+
   end
 
   def checkout_history
-    if @user 
+    if @user
       if params[:page]
         @page = params[:page].to_i
       else
         @page = 0
-      end 
+      end
+
       checkouts_hash = @user.TEMP_checkout_history(@page)
       @checkouts = checkouts_hash['checkouts']
       @more_results = checkouts_hash['more_results']
@@ -81,23 +93,26 @@ class UserController < ApplicationController
     else
       @checkouts = {:error => 'missing parameters'}
     end
+
     respond_to do |format|
       format.json {render :json =>{:user => @user, :checkouts => @checkouts, :page => @page,
                   :more_results => @more_results}}
     end
+
   end
 
 #HOLDS
-
   def holds
     if @user
       @holds = @user.TEMP_get_holds
       basic_info_and_cookies(@user)
     end
+
     respond_to do |format|
       format.html
       format.json {render :json =>{:user => @user, :holds => @holds}}
     end
+
   end
 
   def place_hold
@@ -109,13 +124,15 @@ class UserController < ApplicationController
     else
       @hold = {:error => 'missing parameters'}
     end
+
     respond_to do |format|
       format.js
       format.json {render :json =>{:user => @user, :hold => @hold}}
     end
+
   end
 
-  #valid task params are activate, suspend and #cancel
+  #valid task params are activate, suspend and cancel
   def manage_hold
     if @user && params[:hold_id] && params[:task]
       @holds = @user.TEMP_manage_hold(params[:hold_id], params[:task])
@@ -123,20 +140,24 @@ class UserController < ApplicationController
     else
       @holds = {:error => 'missing parameters'}
     end
+
     respond_to do |format|
       format.json {render :json =>{:user => @user, :holds => @holds}}
     end
+
   end
 
   def change_hold_pickup
-    if @user && params[:hold_id] && params[:hold_status] && params[:pickup_location] 
+    if @user && params[:hold_id] && params[:hold_status] && params[:pickup_location]
       @hold = @user.TEMP_change_hold_pickup(params[:hold_id], params[:hold_status], params[:pickup_location])
     else
       @hold = {:error => 'missing parameters'}
     end
+
     respond_to do |format|
       format.json {render :json => @hold }
-    end    
+    end
+
   end
 
 # FINES
@@ -146,9 +167,11 @@ class UserController < ApplicationController
     else
       @fines = {:error => 'missing parameters'}
     end
+
     respond_to do |format|
       format.json {render :json => @fines}
-    end  
+    end
+
   end
 
   def payments
@@ -157,26 +180,34 @@ class UserController < ApplicationController
     else
       @payments = {:error => 'missing parameters'}
     end
+
     respond_to do |format|
+      format.html
       format.json {render :json => @payments}
-    end  
-  end 
+    end
+
+  end
 
   def preferences
     if @user
       @preferences = @user.TEMP_get_preferences
       basic_info_and_cookies(@user)
     end
+
     respond_to do |format|
-       format.json {render :json =>{:user => @user, :preferences => @preferences}}
+      format.html
+      format.json {render :json =>{:user => @user, :preferences => @preferences}}
     end
+
   end
 
   def missing_token
     @message = {:error=> 'not logged in or invalid token'}
+
     respond_to do |format|
       format.json {render :json => @message}
     end
+
   end
 
 private
