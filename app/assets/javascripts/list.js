b74da05644905efaa701883c0f74724252af9622
William Rockwood <wrockwood@tadl.org>
2018-11-21 16:30:35 -0500

    edit list details
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

---
+++ app/assets/javascripts/list.js
@@ -0,0 +1,18 @@
+function edit_list_details(id) {
+  $('#list-'+id).hide();
+  $('#edit-list-'+id).show();
+}
+
+function cancel_edit_list(id) {
+  $('#edit-list-'+id).hide();
+  $('#list-'+id).show();
+}
+
+function delete_list(element) {
+}
+
+function toggle_list_visibility(element, newstatus) {
+}
+
+function set_list_default(element) {
+}
