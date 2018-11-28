bea4fed9754dd4c4bbec97f69e77b6e0ec4486f0
William Rockwood <wrockwood@tadl.org>
2018-11-28 14:23:56 -0500

    nail down delete list ux, add new list button
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -16,9 +16,14 @@ function save_list_details(id) {
 }
 
 function delete_list(element) {
+  var listid = $(element).data('listid');
+  $(element).attr('onclick', 'actually_delete_list(this)').removeClass('btn-primary').addClass('btn-danger').html('Confirm');
 }
 
-function actually_delete_list(id) {
+function actually_delete_list(element) {
+  var listid = $(element).data('listid');
+  console.log(listid);
+  $(element).html('<i class="fas fa-asterisk spin"></i> Deleting...').attr('disabled', true).addClass('disabled');
 }
 
 function toggle_list_visibility(element, newstatus) {
