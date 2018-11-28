103fbf3e94d406e61f0eaf8778ad0f90ae805932
William Rockwood <wrockwood@tadl.org>
2018-11-28 13:44:07 -0500

    editing list details should be exclusive
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,17 +1,24 @@
 function edit_list_details(id) {
   $('#list-'+id).hide();
   $('#edit-list-'+id).show();
+  $('.list-info').each(function() {
+    $(this).find('button').attr('disabled', true).addClass('disabled');
+  });
 }
 
 function cancel_edit_list(id) {
   $('#edit-list-'+id).hide();
   $('#list-'+id).show();
+  $('button').attr('disabled', false).removeClass('disabled');
 }
 
 function save_list_details(id) {
 }
 
 function delete_list(element) {
+}
+
+function actually_delete_list(id) {
 }
 
 function toggle_list_visibility(element, newstatus) {
