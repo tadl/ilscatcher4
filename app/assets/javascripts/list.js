d1386cd88a823dc5d425c531b25efdd3080bb973
William Rockwood <wrockwood@tadl.org>
2018-11-29 13:24:52 -0500

    list visibility toggle works
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -45,7 +45,19 @@ function actually_delete_list(element) {
   $(element).html('<i class="fas fa-asterisk spin"></i> Deleting...').attr('disabled', true).addClass('disabled');
 }
 
-function toggle_list_visibility(element, newstatus) {
+function toggle_list_visibility(element, visibility) {
+  $(element).html('<i class="fas fa-asterisk spin"></i> Toggling share setting...').attr('disabled', true).addClass('disabled');
+  var listid = $(element).data('listid');
+  var offset = $(element).data('offset');
+  var share = (visibility == 'Public') ? 'show' : 'hide';
+  $.post('/share_list.json', {list_id: listid, offset: offset, share: share})
+    .done(function(data) {
+      if (!data.error) {
+        location.reload()
+      } else {
+        show_alert('danger', 'Something went wrong. Please try again.');
+      }
+    });
 }
 
 function set_list_default(element) {
